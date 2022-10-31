e9db4ca99b116bdbf21105a2f863c98114507b82
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-10-30 21:59:52 -0400

    add the initialized object to a class variable called all

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,16 @@
 class Magazine
   attr_accessor :name, :category
 
-
+  @@all =[]
   def initialize(name, category)
     @name = name
     @category = category
+    @@all >>self
 
   end
 
 
 end
+# magazine1 = Magazine.new("Unbowed","Historical book")
+# magazine1.name
+# magazine1.category
