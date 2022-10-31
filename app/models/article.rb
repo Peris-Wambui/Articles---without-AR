eff49181a6dc96df9d94ab005de139c467fe3f26
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-10-30 22:16:30 -0400

    initialize the class Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article
+    def initialize(author, magazine, title)
+        @author =(author)
+        @magazine =(magazine)
+        @title =(title)
 
 
 end
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,17 @@
 class Magazine
   attr_accessor :name, :category
 
-  @@all =[]
+  @@all = []
   def initialize(name, category)
     @name = name
     @category = category
-    @@all >>self
+    
+    @@all << self
 
+  end
+
+  def self.all
+    @@all
   end
 
 
